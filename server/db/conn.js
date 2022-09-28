594dc764d8dbea6759f9f1b92e0f12443227b9c4
Jerry Ho <itsjerryho@gmail.com>
2022-09-28 23:05:43 +0200

    Minor changes to server config and routes

--- server/db/conn.js
+++ server/db/conn.js
@@ -13,7 +13,7 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("teams");
+        _db = db.db("FootballChampionship");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
